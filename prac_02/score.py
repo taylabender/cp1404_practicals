8a296866e83c5168fdeda880663912206fa5df4f
taylabender <tayla.bender@my.jcu.edu.au>
2024-09-29 12:44:36 +1000

    Refactor score program using functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,12 +2,22 @@
 Refactoring broken scores to include functions
 """
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid")
-elif score < 49:
-    print("Bad")
-elif score < 89:
-    print("passable")
-elif score <= 100:
-    print("Excellent")
+def main():
+    """ get score from input """
+    score = float(input("Enter score: "))
+    determine_grade(score)
+
+
+def determine_grade(score):
+    """ determine grade from score """
+    if score > 100 or score < 0:
+        print("Invalid")
+    elif score < 49:
+        print("Bad")
+    elif score < 89:
+        print("passable")
+    elif score <= 100:
+        print("Excellent")
+
+
+main()
