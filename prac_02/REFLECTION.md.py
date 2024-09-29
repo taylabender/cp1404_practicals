ee4b247858cdc9e433f89cbc844f8b23c01dadcf
taylabender <tayla.bender@my.jcu.edu.au>
2024-09-29 14:09:05 +1000

    Add reflection

---
+++ prac_02/REFLECTION.md.py
@@ -0,0 +1,40 @@
+# CP1404 Practical Reflection
+"""
+Write short but thoughtful answers to each of the following.
+Replace each `...` with your meaningful answer.
+
+## What percentage of the lecture videos have you watched so far?
+- 98% - forgot to watch the last video in Lecture 2B because I was saving it for when I had the time to
+    go through GitHub, then only remembered after struggling for 3 hours getting the program sorted.
+
+
+## What time, location and duration have you scheduled to watch lecture videos?
+- At uni or home on Tuesdays, I have no other lectures on that day.
+
+
+## What are you doing well in the subject so far that you will keep doing?
+- Following the lectures as the lecturer has intended. Stopping and writing
+    code when he suggests rather than skipping it to get through the lectures.
+    Writing the short-cuts for coding down when the lecture mentions them in
+    the videos.
+
+
+## What do you need to stop or change in your work for this subject overall so far?
+- Allowing myself more time to get through the practices. I hope that they
+    only take an hour or two, then I become stressed and annoyed at myself
+    for taking longer.
+
+
+## What are you currently doing poorly for practicals in this subject that you will change?
+- Remembering the functions and how to structure the code properly. I know this comes with
+    repetition, but it's frustrating when I need to look back at other code to see how I did
+    it then. Also, not very good at hand-writing code.
+
+
+## What are you doing really well for practicals in this subject?
+- Not giving up and looking at the solutions straight away. I will try figure it for a good while
+    before checking where I went wrong. And only twice did I end up checking, other times I had
+    managed to figure it out.
+
+
+"""
