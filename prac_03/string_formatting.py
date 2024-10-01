c5fbf5412dfc409525fdc090400c1a218dcc9e27
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-02 09:07:59 +1000

    Completed both TODO

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -14,7 +14,7 @@ cost = 16035.9
 # 1922 Gibson L-5 CES for about $16,036! """
 print(f"{year} {name} for about ${cost:,.0f}!")
 
-# TODO: Using a for loop with the range function and f-string formatting,
+# Using a for loop with the range function and f-string formatting,
 # produce the following right-aligned output (DO NOT use a list):
 # 2 ^ 0 is    1
 # 2 ^ 1 is    2
@@ -27,3 +27,6 @@ print(f"{year} {name} for about ${cost:,.0f}!")
 # 2 ^ 8 is  256
 # 2 ^ 9 is  512
 # 2 ^10 is 1024
+for i in range(11):
+    result = 2**i
+    print(f"2 ^{i:2} is {result:4}")
