c8c82b650673364b0bbf249d2877db2be8e2c27c
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-02 09:39:43 +1000

    Add program and answered questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,24 @@
+"""
+Write the answers to the questions below in comments
+"""
+
+# 10, 19, 18, 19, 12
+# 3, 3, 5, 7, 5
+# 3.721371342126421,4.574224596997082, 5.060368077904657
+
+
+# Line 1
+# The smallest number to see would be 5, with the
+# largest number being 20.
+#
+# Line 2
+# The smallest number to see would be a 3, with the largest
+# number being 9. It would not produce a 4 as the code will only
+# execute odd numbers.
+#
+# Line 3
+# The smallest number to see would be a 2.5, with the largest
+# number being 5.5. The function will return any floating-point
+# numbers between the two values.
+
+
