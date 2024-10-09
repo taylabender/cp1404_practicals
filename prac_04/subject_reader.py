76eb532581797720237eeb075b4f79685fc61da3
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-09 15:40:49 +1000

    Add complete subject reader

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,23 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subjects = load_subjects()
+    for subject in subjects:
+        print("{} is taught by {:12} and has {:3} student".format(*subject))
 
-
-def load_data():
+def load_subjects():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        subject.append(parts)
         print("----------")
     input_file.close()
+    return subject
+
 
 
 main()
