d3b13373c2c6ff79a14b84a012a9747e8b83de86
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-09 12:17:34 +1000

    Complete list_warmups

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,26 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]    = 3
+# numbers[-1]   = 2
+# numbers[3]    = 1
+# numbers[:-1]  = 3, 1, 4, 1, 5, 9
+# numbers[3:4]  = 5, 9, 2
+# 5 in numbers  = True
+# 7 in numbers  = false
+# "3" in numbers= false
+# numbers + [6, 5, 3]  = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+# 1.
+numbers[0] = "ten"
+print(numbers)
+
+# 2.
+numbers[-1] = 1
+print(numbers)
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
