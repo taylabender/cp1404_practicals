1b361df924d99d0398e9d3cfb5e7a5da546b1ee4
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-12 14:08:35 +1000

    Add security checker program

---
+++ prac_04/security_checker.py
@@ -0,0 +1,12 @@
+"""
+Ask user for their username
+"""
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username: ")
+
+if username != usernames:
+    print("Invalid username")
+
