e2e258b7c30773ab84d6144448d1be5890601175
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-12 16:30:26 +1000

    fix traceback error

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,11 +4,12 @@ Lottery ticket generator
 import random
 NUMBERS_PER_LINE = 6
 
+
 try:
        number_of_quick_pick = int(input("Enter the number of quick picks: "))
-       while number_of_quick_pick < 0:
+       if number_of_quick_pick < 0:
               print("Invalid number, please enter positive number")
-              quick_pick = int(input("Enter the number of quick picks: "))
+              number_of_quick_pick = int(input("Enter the number of quick picks: "))
 
        for i in range(number_of_quick_pick):
               quick_pick =[]
@@ -19,6 +20,7 @@ try:
                      quick_pick.append(number)
               quick_pick.sort()
               print(" ".join(f"{number:2d}" for number in quick_pick))
+
 except ValueError:
        print("Invalid input.")
 
