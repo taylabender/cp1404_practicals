f362ec9fcb322c6fd2054695982128f99e7b79a3
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-12 13:46:44 +1000

    Add refactor functions

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,3 +3,28 @@ Prompt the user for 5 numbers and then store
 each of these in a list called 'numbers'.
 """
 
+numbers = []
+
+def main():
+
+    print("Enter 5 numbers below")
+    get_number()
+    average = sum(numbers) / len(numbers)
+    print_list(average)
+
+
+def print_list(average):
+    print(f"The first number is {numbers[0]:.0f}")
+    print(f"The last number is {numbers[-1]:.0f}")
+    print(f"The smallest number is {min(numbers):.0f}")
+    print(f"The largest number is {max(numbers):.0f}")
+    print(f"The average of the numbers is {average:.1f}")
+
+
+def get_number():
+    for i in range(5):
+        number = float(input(f"Number {i + 1}: "))
+        numbers.append(number)
+
+
+main()
