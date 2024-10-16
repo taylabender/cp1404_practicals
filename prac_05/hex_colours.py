3221491c11add44a7196057f1a1292b540867d06
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-16 18:40:19 +1000

    Print colours and codes

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -6,5 +6,9 @@ CODE_TO_NAME = {"#00ffff": "Aqua", "#ffbf00": "Amber", "#fbceb1": "Apricot",
                 "#89cff0": "Baby Blue", "3ffee135": "Banana Yellow", "#ff7f50": "Coral",
                 "#fffdd0": "Cream", "350c878": "Emerald", "#4b0082": "Indigo"}
 
-print(CODE_TO_NAME)
+colour_code = list(CODE_TO_NAME.keys())
+colour_name = list(CODE_TO_NAME.values())
+max_length = max(len(colour_name) for colour_name in list(CODE_TO_NAME.values()))
+for colour_code, colour_name in CODE_TO_NAME.items():
+    print(f"{colour_name:{max_length}} is {colour_code}")
 
