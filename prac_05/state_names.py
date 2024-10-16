a74ea2642fcbafab694dfc62ab849e1eb8afd514
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-16 10:18:26 +1000

    Add loop to print all states

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,11 +9,19 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+short_state_name = list(CODE_TO_NAME.keys())
+long_state_name = list(CODE_TO_NAME.values())
+for short_state_name, long_state_name in CODE_TO_NAME.items():
+    print(f"{short_state_name:3} is {long_state_name}")
+print("-------------------")
+
 state_code = input("Enter short state: ").upper() #convert input to uppercase
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
+
 
