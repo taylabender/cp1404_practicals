1cc331ce0f0d37c2b5314a59813a23ae826f93d5
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-16 18:35:00 +1000

    Add code_to_name for hex colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,10 @@
+"""
+program to look up hexadecimal colours
+"""
+
+CODE_TO_NAME = {"#00ffff": "Aqua", "#ffbf00": "Amber", "#fbceb1": "Apricot",
+                "#89cff0": "Baby Blue", "3ffee135": "Banana Yellow", "#ff7f50": "Coral",
+                "#fffdd0": "Cream", "350c878": "Emerald", "#4b0082": "Indigo"}
+
+print(CODE_TO_NAME)
+
