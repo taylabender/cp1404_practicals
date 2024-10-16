edcd2009110e1538a02b6b187219f6932919cc6a
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-16 19:31:11 +1000

    Add while loop for colour program. Need to fix code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,13 +2,23 @@
 program to look up hexadecimal colours
 """
 
-CODE_TO_NAME = {"#00ffff": "Aqua", "#ffbf00": "Amber", "#fbceb1": "Apricot",
-                "#89cff0": "Baby Blue", "3ffee135": "Banana Yellow", "#ff7f50": "Coral",
-                "#fffdd0": "Cream", "350c878": "Emerald", "#4b0082": "Indigo"}
+CODE_TO_COLOUR = {"#00ffff": "aqua", "#ffbf00": "amber", "#fbceb1": "apricot",
+                "#89cff0": "babyblue", "3ffee135": "bananayellow", "#ff7f50": "coral",
+                "#fffdd0": "cream", "350c878": "emerald", "#4b0082": "indigo"}
 
-colour_code = list(CODE_TO_NAME.keys())
-colour_name = list(CODE_TO_NAME.values())
-max_length = max(len(colour_name) for colour_name in list(CODE_TO_NAME.values()))
-for colour_code, colour_name in CODE_TO_NAME.items():
-    print(f"{colour_name:{max_length}} is {colour_code}")
+colour_code = list(CODE_TO_COLOUR.keys())
+colour_name = list(CODE_TO_COLOUR.values())
+for colour_code, colour_name in CODE_TO_COLOUR.items():
+    print(f"{colour_name}")
+
+print("-------------------")
+print("Please enter colour to look up hexadecimal code")
+
+while colour_name != CODE_TO_COLOUR:
+    get_colour_name = input("\nEnter colour name here: ").strip().lower()
+    if get_colour_name in CODE_TO_COLOUR:
+         print(get_colour_name, "is", CODE_TO_COLOUR[colour_code])
+    else:
+        print(get_colour_name, "is not in the database.")
+
 
