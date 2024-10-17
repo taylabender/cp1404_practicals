0e11598d52d4e37e5148e53038353cf7fc5c18cf
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 08:58:43 +1000

    Add for loop to word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,5 +3,13 @@ Estimate: 25 minutes
 Actual:
 """
 
+sentence = input("Enter a sentence: ")
+word_count = {}
+
+for word in sentence.lower().split():
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
 
 
