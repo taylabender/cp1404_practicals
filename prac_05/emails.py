f2c7e6fc8b66c18eecf37757dadc846dbc9e526d
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 09:32:39 +1000

    Ask for email and confirm name for emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,3 +1,14 @@
 """
 Program to store users' email addresses into a dictionary.
+Estimate time: 30 minutes
+Actual time:
 """
+
+name_from_email = {}
+email = input("Enter your email address: ").strip()
+
+username = email.split('@')[0]
+parts = username.split('.')
+suggested_name = " ".join(parts).title()
+
+confirmation = input(f"Is your name {suggested_name}? (Y/N) ").lower
