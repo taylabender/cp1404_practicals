49bad676ac3ea4a27a70729ee9852c6c51fa66a5
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 09:07:59 +1000

    Refactor word occurrence program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,15 +6,26 @@ Actual:
 sentence = input("Enter a sentence: ")
 word_count = {}
 
-for word in sentence.lower().split():
-    if word in word_count:
-        word_count[word] += 1
-    else:
-        word_count[word] = 1
 
-longest_word = max(len(word) for word in word_count.keys())
+def main():
 
-for word in sorted(word_count.keys()):
-    print(f"{word:{longest_word}}: {word_count[word]}")
+    count_words()
+    longest_word = max(len(word) for word in word_count.keys())
+    sort_and_print(longest_word)
 
+
+def sort_and_print(longest_word):
+    for word in sorted(word_count.keys()):
+        print(f"{word:{longest_word}}: {word_count[word]}")
+
+
+def count_words():
+    for word in sentence.lower().split():
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+
+main()
 
