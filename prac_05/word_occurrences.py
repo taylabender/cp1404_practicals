bdfee052768bd92f21dd8bc40f55d5384afff9cf
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 09:11:02 +1000

    Add comments for word occurrence program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,18 +8,19 @@ word_count = {}
 
 
 def main():
-
     count_words()
     longest_word = max(len(word) for word in word_count.keys())
     sort_and_print(longest_word)
 
 
 def sort_and_print(longest_word):
+    """ sort words and print word count """
     for word in sorted(word_count.keys()):
         print(f"{word:{longest_word}}: {word_count[word]}")
 
 
 def count_words():
+    """ count occurrences of words in sentence """
     for word in sentence.lower().split():
         if word in word_count:
             word_count[word] += 1
