da14f88082d78519cc05af88ed3644501afb3287
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 09:04:40 +1000

    Complete word occurrence program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -12,4 +12,9 @@ for word in sentence.lower().split():
     else:
         word_count[word] = 1
 
+longest_word = max(len(word) for word in word_count.keys())
+
+for word in sorted(word_count.keys()):
+    print(f"{word:{longest_word}}: {word_count[word]}")
+
 
