664d8e7c2d5a7b7010205907948c83ee22f4ba26
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 12:12:16 +1000

    Add wimbledon program

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,10 @@
+"""
+Wimbledon champions data
+"""
+
+# Read and analyse CSV file
+# Create dictionary of champions and their wins
+# Display champions and their wins
+# Get unique countries and sort them
+# convert set to sorted list
+# Display countries
