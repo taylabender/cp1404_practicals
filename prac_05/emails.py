b5087ecad89c7fe8e51280c348d28efc4428ce1a
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 14:17:54 +1000

    Fix broken loop in emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,35 +4,30 @@ Estimate time: 30 minutes
 Actual time:
 """
 def main():
-
-
     name_from_email = {}
     email = input("Enter your email address: ")
 
-    while email != "":
-        username = email.split('@')[0]
-        parts = username.split('.')
-        suggested_name = " ".join(parts).title()
-
+    while True:
+        suggested_name = get_name_from_email(email)
         confirmation = input(f"Is your name {suggested_name}? (Y/N) ")
+
         if confirmation.upper() != "Y":
             name = input("Enter your name: ")
-            name_from_email[email] = name # stores the entered name
+            print(f"{name}'s email address is {email}")
         else:
-            name_from_email[email] = suggested_name # stores the suggested name
+            name_from_email[email] = suggested_name  # Store the suggested name
 
-        email = input("Enter your email address: ")
+        for email, name in name_from_email.items():
+            print(f"{name}'s email address is {email}")
 
-    for email, name in name_from_email.items():
-        print(f"{name} email address is {email}")
+
+def get_name_from_email(email):
+    username = email.split('@')[0]
+    parts = username.split('.')
+    suggested_name = " ".join(parts).title()
+    return suggested_name
+
 
 main()
-
-
-    # name = suggested_name
-    # print(f"{name}'s email address is {email}")
-# else:
-#     name_from_email[email] = suggested_name
-
 
 # tayla.bender@outlook.com
