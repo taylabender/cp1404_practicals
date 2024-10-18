1d004a87ed71deb7a5891478a015e1eb18be9fe0
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 14:20:50 +1000

    Fix state names program

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,20 +6,17 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-short_state_name = list(CODE_TO_NAME.keys())
-long_state_name = list(CODE_TO_NAME.values())
 for short_state_name, long_state_name in CODE_TO_NAME.items():
     print(f"{short_state_name:3} is {long_state_name}")
+
 print("-------------------")
 
-state_code = input("Enter short state: ").upper() #convert input to uppercase
-while state_code != "":
-   try:
-       print(state_code, "is", CODE_TO_NAME[state_code])
+state_code = input("\nEnter short state: ").upper() # Convert input to uppercase
+while True:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
     except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
-
 
