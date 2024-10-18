dcba65bbbbe92be54d36aa94f5283cfa4ad3e013
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-18 12:25:50 +1000

    Open and read wimbledon file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,8 +3,19 @@ Wimbledon champions data
 """
 
 # Read and analyse CSV file
+FILENAME = 'wimbledon.csv'
+
+records = []
+with open(FILENAME, 'r', encoding="utf-8-sig") as in_file:
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split(",")
+        records.append(parts)
+print(records)
+
 # Create dictionary of champions and their wins
 # Display champions and their wins
 # Get unique countries and sort them
 # convert set to sorted list
 # Display countries
+
