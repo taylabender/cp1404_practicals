eb02934cc0335d930cba2a79f7b1c902f07dae79
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-30 19:53:12 +1000

    Add init function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+"""
+Estimate time: 30 min
+Actual time:
+"""
+
+class Language:
+    def __init__(self, name="", typing="", reflection="True", year=0):
+        """Initialise programming language.
+        name: Name of string
+        typing: Static or Dynamic
+        reflection: True or False
+        year: Year
+        """
+
+
+
