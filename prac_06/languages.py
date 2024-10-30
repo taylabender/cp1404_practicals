dd10e78490eae0dca9b70471d2e7c701875d4410
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-30 20:08:20 +1000

    Print all languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,4 +3,8 @@ from prac_06.programming_language import ProgrammingLanguage
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
+
+# Print list of languages
+# print(python)
+
+print(f"{python} \n{ruby} \n{visual_basic}")
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,10 +14,10 @@ class ProgrammingLanguage:
         self.name = name
         self.typing = typing
         self.reflection = reflection
-        self.year = 0
+        self.year = year
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
 
     def __str__(self):
-        return f"{self.name} {self.typing} {self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} typing, Reflection={self.reflection}, First appeared in {self.year}"
