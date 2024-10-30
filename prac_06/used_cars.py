acabee34b6ee20697527710d8441f0ea573ff825
taylabender <tayla.bender@my.jcu.edu.au>
2024-10-30 19:07:24 +1000

    Add Limo to used cars program

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -12,7 +12,12 @@ def main():
     my_car = Car(180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
+    # print(my_car)
+
+    limo = Car(100)
+    limo.add_fuel(20)
+    limo.drive(115)
+    print(f"Limo has fuel: {limo.fuel}")
 
 
 main()
