d6bbc68e53ce9c6584848ab548e73ddb3b0d62fe
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-01 08:39:27 +1000

    Add guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,3 +2,14 @@
 Estimate time: 30 min
 Actual time:
 """
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise programming language.
+            name: Name of guitar
+            year: Year
+            cost: Cost of guitar
+            """
+        self.name = name
+        self.year = year
+        self.cost = cost
