3dd92f7b30b567581c0c4c27eca99530cfbb853b
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-01 12:14:48 +1000

    Refactor user input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,13 +7,20 @@ def main():
     # Get guitar details from user
     name = input("Guitar Name: ")
     while name != "":
-        year = int(input("Enter a year: "))
-        cost = float(input("Enter a cost: "))
-        add_guitar = Guitar(name, year, cost)
-        guitars.append(add_guitar)
-        print(f"Guitar added: {add_guitar.name}")
-        print()
+        add_guitar = get_user_input(guitars, name)
+        print(f"{add_guitar.name} ({add_guitar.year}) : ${add_guitar.cost} added.")
+        name = input(f"\nGuitar Name: ")
 
+    if guitars:
+        print("\nThese are my guitars:")
+
+
+def get_user_input(guitars, name):
+    year = int(input("Enter a year: "))
+    cost = float(input("Enter a cost: "))
+    add_guitar = Guitar(name, year, cost)
+    guitars.append(add_guitar)
+    return add_guitar
 
 
 main()
