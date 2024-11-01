05a6cff34d83607a8767e36cb1fe3e99cb55be64
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-01 11:44:43 +1000

    Add docstrings in class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -15,13 +15,16 @@ class Guitar:
         self.cost = cost
 
     def get_age(self):
+        """ Determine age of guitar. """
         year = self.year
         current_year = 2024
         return current_year - year
 
     def is_vintage(self):
-         return self.get_age() >= 50
+        """Determine whether guitar is vintage."""
+        return self.get_age() >= 50
 
 
     def __str__(self):
+        """Return a string representation of guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:.2f}"
