439142560439b77beffad40927a8e2b27e10c160
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-01 12:25:57 +1000

    Print sample user input lines.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,26 +1,35 @@
+from prac_06 import guitar
 from prac_06.guitar import Guitar
 
 def main():
     print("My guitars!")
     guitars = []
 
-    # Get guitar details from user
-    name = input("Guitar Name: ")
-    while name != "":
-        add_guitar = get_user_input(guitars, name)
-        print(f"{add_guitar.name} ({add_guitar.year}) : ${add_guitar.cost} added.")
-        name = input(f"\nGuitar Name: ")
+    # Test data
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
 
+
+    # # Get guitar details from user
+    # name = input("Guitar Name: ")
+    # while name != "":
+    #     add_guitar = get_user_input(guitars, name)
+    #     print(f"{add_guitar.name} ({add_guitar.year}) : ${add_guitar.cost} added.")
+    #     name = input(f"\nGuitar Name: ")
+
+    # Display guitars
     if guitars:
         print("\nThese are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year:>4}), worth $ {guitar.cost:>9,.2f}")
 
-
-def get_user_input(guitars, name):
-    year = int(input("Enter a year: "))
-    cost = float(input("Enter a cost: "))
-    add_guitar = Guitar(name, year, cost)
-    guitars.append(add_guitar)
-    return add_guitar
+# def get_user_input(guitars, name):
+#     year = int(input("Enter a year: "))
+#     cost = float(input("Enter a cost: "))
+#     add_guitar = Guitar(name, year, cost)
+#     guitars.append(add_guitar)
+#     return add_guitar
 
 
 main()
