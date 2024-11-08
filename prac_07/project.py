1d44a8abf37e05a612bfd1eec5df44bda861d430
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-08 14:45:16 +1000

    Reformat and add datetime

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,19 @@
+"""
+Project class
+"""
+from datetime import datetime
+
+
 
 class Project:
-    def __init__(self, name="", start_date="", priority=0, cost=0.0, completion_percentage=100):
+    def __init__(self, name="", start_date="", priority=0, cost=0.0, percentage=100):
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost = cost
-        self.completion_percentage = completion_percentage
+        self.percentage = percentage
 
+    def __repr__(self):
+        """Return a string representation of project."""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, Estimate: ${self.cost:.2f}, completion:{self.percentage:}%"
 
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,29 +4,31 @@ Actual time:
 """
 
 from prac_07.project import Project
-import csv
+from datetime import datetime
 
 FILENAME = 'projects.txt'
-projects = []
+
+# Display menu
+menu = ("(L)oad projects"
+        "\n(S)ave projects"
+        "\n(D)isplay projects"
+        "\n(F)ilter projects by date"
+        "\n(A)dd new project"
+        "\n(U)pdate project"
+        "\n(Q)uit")
+
 
 def main():
-    # Open filename
-    with open(FILENAME, 'r', newline='') as in_file:
+    projects = []
+
+    # Load file
+    with open(FILENAME, 'r') as in_file:
         in_file.readline()
-        reader = csv.reader(in_file)
-        for row in reader:
-            projects.append(Project(row[0], (row[1]), int(row[2]), float(row[3]), int(row[4])))
-
-    # Display menu
-    menu = ("(L)oad projects"
-            "\n(S)ave projects"
-            "\n(D)isplay projects"
-            "\n(F)ilter projects by date"
-            "\n(A)dd new project"
-            "\n(U)pdate project"
-            "\n(Q)uit")
+        for row in in_file:
+            row = row.split("\t")
+            projects.append(Project(row[0], row[1], int(row[2]), float(row[3]), int(row[4])))
+    
     print(menu)
-
     choice = input("<<<").upper()
     while choice != "Q":
         if choice == "L":
