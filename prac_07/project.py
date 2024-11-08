b3c4da798e7524e90c7f34b3fb4f8682c2de580f
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-08 14:19:14 +1000

    Open project file.

---
+++ prac_07/project.py
@@ -0,0 +1,10 @@
+
+class Project:
+    def __init__(self, name="", start_date="", priority=0, cost=0.0, completion_percentage=100):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion_percentage = completion_percentage
+
+
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,3 +2,19 @@
 Estimate time: 45min
 Actual time:
 """
+
+from prac_07.project import Project
+import csv
+
+FILENAME = 'projects.txt'
+projects = []
+
+def main():
+    # Open filename
+    with open(FILENAME, 'r', newline='') as in_file:
+        in_file.readline()
+        reader = csv.reader(in_file)
+        for row in reader:
+            projects.append(Project(row[0], (row[1]), int(row[2]), float(row[3]), int(row[4])))
+
+
