f86305c75e3a18f4654f49d5beef0848133d77eb
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-08 14:26:26 +1000

    Add menu

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -17,4 +17,32 @@ def main():
         for row in reader:
             projects.append(Project(row[0], (row[1]), int(row[2]), float(row[3]), int(row[4])))
 
+    # Display menu
+    menu = ("(L)oad projects"
+            "\n(S)ave projects"
+            "\n(D)isplay projects"
+            "\n(F)ilter projects by date"
+            "\n(A)dd new project"
+            "\n(U)pdate project"
+            "\n(Q)uit")
+    print(menu)
 
+    choice = input("<<<").upper()
+    while choice != "Q":
+        if choice == "L":
+            break
+        if choice == "S":
+            break
+        if choice == "D":
+            break
+        if choice == "F":
+            break
+        if choice == "A":
+            break
+        if choice == "U":
+            break
+        else:
+            break
+
+
+main()
