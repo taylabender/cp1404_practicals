1df5ee8de611e39b337474803977f3982adc971f
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-22 13:22:33 +1000

    Add taxi test

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,7 +2,20 @@
 Prac 09 - Testing Taxi
 """
 
-from prac_09.car import Car
 from prac_09.taxi import Taxi
 
+def main():
+    """ Testing Taxi class. """
+    taxi = Taxi(name="Prius 1", fuel=100, price_per_km=1.23)
+    taxi.drive(40)
+    print(taxi)
+    print(taxi.get_fare())
 
+    taxi.start_fare() # Start new fare
+    taxi.drive(100)
+    print(taxi)
+    print(taxi.get_fare())
+
+
+
+main()
