650eb44695f8837c6be81c199bfb4e1f599dba3c
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-22 14:13:23 +1000

    Complete silver service taxi

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,7 +9,7 @@ class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness: float):
-        super.__init__(name, fuel)
+        super().__init__(name, fuel, price_per_km=1.23)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * fanciness
 
