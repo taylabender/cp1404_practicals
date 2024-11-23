e36f9471b8437bf38cfe98aad419aa62c30b45cf
taylabender <tayla.bender@my.jcu.edu.au>
2024-11-23 10:03:23 +1000

    Error check for drive taxi, and refactor

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -13,7 +13,7 @@ MENU_LIST = ("q)uit, "
 def main():
     current_taxi = None
     current_bill = 0.0
-    taxi = [Taxi("Prius", 100, 1.23), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    taxis = [Taxi("Prius", 100, 1.23), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
     print(MENU_LIST)
 
@@ -21,33 +21,49 @@ def main():
     choice = input(">>").upper()
     while choice != "Q":
         if choice == "C":
-            print("Taxi's available: ")
-            for i, taxi in enumerate(taxi):
-                print(f"{i} - {taxi}")
-        try:
-            chosen_taxi = int(input("Choose taxi: "))
-            current_taxi = taxi[chosen_taxi]
-        except ValueError:
-            print("Not a valid number")
-        except IndexError:
-            print("Invalid option")
-
-        if choice == "D":
-            if current_taxi is None:
-                print("You need to choose a taxi before you drive")
-            else:
-                distance = float(input("Drive how far?"))
-                current_taxi.start_fare()
-                current_taxi.drive(distance)
-                cost = current_taxi.get_fare()
-                print(f"Your {current_taxi.name} trip cost you: ${cost:.2f}")
-                current_bill += cost
+            current_taxi = choose_taxi(current_taxi, taxis)
+        elif choice == "D":
+            current_bill = drive_taxi(current_bill, current_taxi)
         else:
             print("Invalid option")
         print(f"Bill to date: ${current_bill:.2f}")
-        print("Taxis are now:")
-        for i, taxi in enumerate(taxi):
-            print(f"{i} - {taxi}")
+        print(MENU_LIST)
+        choice = input(">>").upper()
+
+    print(f"Bill to date: ${current_bill:.2f}")
+    print("Taxis are now:")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def drive_taxi(current_bill, current_taxi):
+    if current_taxi is None:
+        print("You need to choose a taxi before you drive")
+    else:
+        try:
+            distance = float(input("Drive how far?"))
+            current_taxi.start_fare()
+            current_taxi.drive(distance)
+            cost = current_taxi.get_fare()
+            print(f"Your {current_taxi.name} trip cost you: ${cost:.2f}")
+            current_bill += cost
+        except ValueError:
+            print("Not a valid number")
+    return current_bill
+
+
+def choose_taxi(current_taxi, taxis):
+    print("Taxis available: ")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+    try:
+        chosen_taxi = int(input("Choose taxi: "))
+        current_taxi = taxis[chosen_taxi]
+    except ValueError:
+        print("Not a valid number!")
+    except IndexError:
+        print("Invalid option!")
+    return current_taxi
 
 
 main()
