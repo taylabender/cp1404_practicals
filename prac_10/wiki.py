be7dc9d9a05cd9bc3aa683935901de6b0e2b2d41
taylabender <tayla.bender@my.jcu.edu.au>
2024-12-01 14:51:52 +1000

    Print summary and url or page title.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,9 +4,13 @@ Prac 10
 
 import wikipedia
 
-internet_search = input("Search Wikipedia: ")
+internet_search = input("Enter page title: ")
 while internet_search != "":
     results = wikipedia.page(internet_search)
     print(results.title)
-    internet_search = input("Search Wikipedia: ")
+    print(results.summary)
+    print(results.url)
+    internet_search = input("Enter page title: ")
+
+print("Thank you!")
 
