de47e2bc53d593b10c5f95f8d4ab2a825e836d09
taylabender <tayla.bender@my.jcu.edu.au>
2024-12-01 14:45:20 +1000

    Add while loop for user input.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,6 +2,11 @@
 Prac 10
 """
 
-
 import wikipedia
+
+internet_search = input("Search Wikipedia: ")
+while internet_search != "":
+    results = wikipedia.page(internet_search)
+    print(results.title)
+    internet_search = input("Search Wikipedia: ")
 
