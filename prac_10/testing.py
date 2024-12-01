92e8511c7d6aec310a76edc4e925f692ce962764
taylabender <tayla.bender@my.jcu.edu.au>
2024-12-01 13:58:10 +1000

    Convert hello to Hello.

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -41,16 +41,17 @@ def run_tests():
     test_car = Car(fuel=10)
     assert test_car.fuel == 10, "Car does not set fuel correctly when passing the value i"
 
+def convert_phrase_to_sentence(phrase):
+    """
+    >>> convert_phrase_to_sentence('hello')
+    'Hello.'
+
+    """
+    if phrase[-1] != ".":
+        phrase += "."
+    return phrase.capitalize()
 
 
-run_tests()
-
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
-doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (Don't change the tests, change the function!)
 
 # TODO: 5. Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
@@ -61,3 +62,10 @@ doctest.testmod()
 # and one more that you decide is a useful test.
 # Run your doctests and watch the tests fail.
 # Then write the body of the function so that the tests pass.
+
+# doctest.testmod()
+
+run_tests()
+
+
+
